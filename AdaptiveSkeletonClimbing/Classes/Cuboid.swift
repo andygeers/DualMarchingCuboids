821956b7b4fc91b652b9f509a5ae5bad5aed779e
Andy Geers <andy.geers@googlemail.com>
2020-08-27 07:00:05 +0100

    Add a cuboid struct

---
+++ AdaptiveSkeletonClimbing/Classes/Cuboid.swift
@@ -0,0 +1,34 @@
+//
+//  Cuboid.swift
+//  AdaptiveSkeletonClimbing
+//
+//  Created by Andy Geers on 27/08/2020.
+//
+
+import Foundation
+import Euclid
+
+struct Cuboid {
+//    let x : Int
+//    let y : Int
+//    let z : Int
+    let index : Int // Can derive x, y, z from this
+    
+    let width : Int
+    let height : Int
+    let depth : Int
+    
+    let vertex1 : Vector
+    let vertex2 : Vector
+    let vertex3 : Vector
+    let vertex4 : Vector
+    
+    // There may of course be a lot of nodes along each edge, for large cuboids,
+    // but we can find all of the others by traversing from the first one on that edge
+    let upNodeIndex : Int
+    let rightNodeIndex : Int
+    let downNodeIndex : Int
+    let leftNodeIndex : Int
+    let forwardsNodeIndex : Int
+    let backwardsNodeIndex : Int
+}
