f15a88ce29b8c3459923d79cef2f0ef09d71f1c6
Andy Geers <andy.geers@googlemail.com>
2020-10-05 07:39:51 +0100

    WIP Allow populating voxels for a building

--- DualMarchingCuboids/Classes/VoxelGrid.swift
+++ DualMarchingCuboids/Classes/VoxelGrid.swift
@@ -7,6 +7,8 @@
 
 import Euclid
 
+let epsilon = 1e-6
+
 enum VoxelAxis : Int {
     case none = 0
     case xy = 1
@@ -32,11 +34,70 @@ public class VoxelGrid {
     
     public var uglyCubes : [Int] = []
     
+    public static let SCALE_FACTOR = 500.0
+    
     public init(width : Int, height : Int, depth : Int) {
         self.width = width
         self.height = height
         self.depth = depth
         self.data = [Int](repeating: 0, count: width * height * depth)
+    }
+    
+    public convenience init(bounds: Bounds) {
+        self.init(width: Int(bounds.size.x * VoxelGrid.SCALE_FACTOR) + 2,
+                  height: Int(bounds.size.y * VoxelGrid.SCALE_FACTOR) + 2,
+                  depth: Int(bounds.size.z * VoxelGrid.SCALE_FACTOR) + 2)
+    }
+    
+    public extension Plane {
+        var pointOnPlane : Vector {
+            if (self.normal.z != 0.0) {
+                let x = 0.0
+                let y = 0.0
+                let z = self.w / self.normal.z
+                return Vector(x, y, z)
+            } else if (self.normal.y != 0.0) {
+                let x = 0.0
+                let y = self.w / self.normal.y
+                let z = 0.0
+                return Vector(x, y, z)
+            } else {
+                let x = self.w / self.normal.x
+                let y = 0.0
+                let z = 0.0
+                return Vector(x, y, z)
+            }
+        }
+    }
+    
+    public func slice(plane: Plane, bounds: Bounds) -> Slice? {
+        // See what kind of slice we want
+        if (abs(plane.normal.y) < epsilon) {
+            if (abs(plane.normal.x) < epsilon) {
+                if (plane.normal.z > 0) {
+                    let z = plane.w / plane.normal.z
+                    
+                    return XYSlice(grid: self, z: Int(z * VoxelGrid.SCALE_FACTOR) + 1)
+                } else {
+                    // TODO: Reverse XY slice
+                    return nil
+                }
+            } else if (abs(plane.normal.z) < epsilon) {
+                if (plane.normal.x > 0) {
+                    let x = plane.w / plane.normal.x
+                    
+                    return YZSlice(grid: self, x: Int(x * VoxelGrid.SCALE_FACTOR) + 1)
+                } else {
+                    // TODO: Revert YZ slice
+                    return nil
+                }
+            } else {
+                return nil
+            }
+        } else {
+            // For now we only support 'vertical' panels
+            return nil
+        }
     }
     
     public func cellIndex(x: Int, y: Int, z: Int) -> Int {
